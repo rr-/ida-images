f18d45cad1f38b4880c3a4c3b762df0f45c4e652
Marcin Kurczewski <rr-@sakuya.pl>
2018-10-18 08:56:31 +0300

    Update librgb/renderer.py

--- librgb/renderer.py
+++ librgb/renderer.py
@@ -52,7 +52,7 @@ class Renderer(object):
         stride = params.width * channels
         data_size = params.height * stride
         if params.format == PixelFormats.GRAY1:
-            data_size = data_size / 8
+            data_size /= 8
 
         data = reader.get_padded_bytes(data_size)
         assert data is not None
